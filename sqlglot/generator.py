f6a1d56067587ed1dcf2a329def8fa9c6c4eddd0
ErwinSiegers <149155415+ErwinSiegers@users.noreply.github.com>
2024-02-17 17:28:10 +0100

    Update generator.py

--- sqlglot/generator.py
+++ sqlglot/generator.py
@@ -533,7 +533,6 @@ class Generator(metaclass=_Generator):
         if copy:
             expression = expression.copy()
         expression = self.preprocess(expression)
-        print(expression)
         self.unsupported_messages = []
         sql = self.sql(expression).strip()
         print(sql)
